7f9dea584261acb9913719f3bd64c402a93b76dc
Valerie Wood <valeriewd1@gmail.com>
2018-01-14 22:23:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = new Object({
+  "Slowdive": "Alison",
+  "My Bloody Valentine": "Sometimes",
+});
+
+
+function updatePlaylist(playlist, artist, song){
+ playlist['Phil Ochs'] = song;
+
+  return playlist;
+}
+
